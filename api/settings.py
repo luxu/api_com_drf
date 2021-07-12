38d832578997b0e90fef7bc8748e32c664257e6b
Luciano Martins <zicadopv@gmail.com>
2021-07-12 08:19:04 -0300

    retirando o deploy automático para o Heroku e setando o sqlite como banco principal

--- api/settings.py
+++ api/settings.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from pathlib import Path
 
-import dj_database_url
 from decouple import Csv, config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,18 +60,18 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
-DATABASES = {}
-DATABASES["default"] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True, default=config("DATABASE_URL")
-)
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# DATABASES = {}
+# DATABASES["default"] = dj_database_url.config(
+#     conn_max_age=600, ssl_require=True, default=config("DATABASE_URL")
+# )
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation
