e153bd7fb29f88c5b548ecd5432aae35b4752fae
Luciano Martins <zicadopv@gmail.com>
2021-07-15 10:07:52 -0300

    especificando para passar nas urls o <id> do estudante

--- api/settings.py
+++ api/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from decouple import Csv, config
 
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = config("DEBUG", cast=bool)
@@ -60,18 +62,18 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-# DATABASES = {}
-# DATABASES["default"] = dj_database_url.config(
-#     conn_max_age=600, ssl_require=True, default=config("DATABASE_URL")
-# )
-# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True, default=config("DATABASE_URL")
+)
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation
